6976adbcfc22cce4c5b374f1685c3f48f7efbf81
ShotaGhoona <shota.yamashita@ghoona.com>
2025-06-29 20:00:28 +0900

    最初のコミット

---
+++ src/components/featuture/lp/aboutSection.tsx
@@ -0,0 +1,7 @@
+export default function AboutSection() {
+  return (
+    <div>
+      <h1>AboutSection</h1>
+    </div>
+  );
+}
---
+++ src/components/featuture/lp/problemSectio.tsx
@@ -0,0 +1,7 @@
+export default function ProblemSection() {
+  return (
+    <div>
+      <h1>Problem Section</h1>
+    </div>
+  );
+}
